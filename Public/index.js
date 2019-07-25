a10f8cbf8897002b0f6543cd33b237b0b3b2401e
Luis Hernandez <luauhern@gmail.com>
2019-07-25 07:32:16 -0400

    added google maps API to allow for user location input

--- Public/index.js
+++ Public/index.js
@@ -1,22 +1,91 @@
 let lat, lon;
 
+const loc = document.getElementById('w-location');
+const desc = document.getElementById('w-desc');
+const temp = document.getElementById('w-string');
+const hum = document.getElementById('w-humidity');
+const vis = document.getElementById('w-visibility');
+const wind = document.getElementById('w-wind-speed');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+const save = document.getElementById('save');
+const modal = document.getElementById('locModal');
+
+loadEventListeners();
+function loadEventListeners() {
+  save.addEventListener('click', getCoordinates);
+}
+
 if ('geolocation' in navigator) {
   console.log('geolocation available');
   navigator.geolocation.getCurrentPosition(async position => {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    console.log(lat, lon);
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
     const api_url = `weather/${lat},${lon}`;
     const response = await fetch(api_url);
     const json = await response.json();
 
-    console.log(json);
-
+    let location = json.timezone;
+    let mlocation = location.substring(8);
     let temperature = json.currently.temperature;
     let summary = json.currently.summary;
-    document.getElementById('summary').textContent = summary;
-    document.getElementById('temperature').textContent = temperature;
+    let humidity = json.currently.humidity;
+    let visibility = json.currently.visibility;
+    let windSpeed = json.currently.windSpeed;
+
+    loc.textContent = mlocation;
+    desc.textContent = summary;
+    temp.textContent = `${temperature} F`;
+    hum.textContent = `Humidity: ${humidity}`;
+    vis.textContent = `Visibility: ${visibility}`;
+    wind.textContent = `Wind Speed: ${windSpeed} `;
+
+    const data = { lat, lon, temperature, summary };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    await fetch('/api', options);
+  });
+}
+
+async function getCoordinates(e) {
+  if (city.value === '' || state.value === '') {
+    alert('Please enter a city and state!');
+  } else {
+    cityInput = city.value;
+    stateInput = state.value;
+    const gapi_url = `coordinates/${cityInput},${stateInput}`;
+    const response = await fetch(gapi_url);
+    const json = await response.json();
+    console.log(json);
+
+    const lat = json.results[0].geometry.location.lat;
+    const lon = json.results[0].geometry.location.lng;
+
+    const api_url = `weather/${lat},${lon}`;
+    const wresponse = await fetch(api_url);
+    const wjson = await wresponse.json();
+    console.log(wjson);
+
+    let location = json.results[0].formatted_address;
+    let temperature = wjson.currently.temperature;
+    let summary = wjson.currently.summary;
+    let humidity = wjson.currently.humidity;
+    let visibility = wjson.currently.visibility;
+    let windSpeed = wjson.currently.windSpeed;
+
+    loc.textContent = location;
+    desc.textContent = summary;
+    temp.textContent = `${temperature} F`;
+    hum.textContent = `Humidity: ${humidity}`;
+    vis.textContent = `Visibility: ${visibility}`;
+    wind.textContent = `Wind Speed: ${windSpeed} `;
 
     const data = { lat, lon, temperature, summary };
     console.log(data);
@@ -28,10 +97,9 @@ if ('geolocation' in navigator) {
       },
       body: JSON.stringify(data)
     };
-    const responseData = await fetch('/api', options);
-    const jsonData = await responseData.json();
-    console.log(jsonData);
-  });
-} else {
-  console.log('geolocation not available');
+    await fetch('/api', options);
+  }
+  city.value = '';
+  state.value = '';
+  e.preventDefault();
 }
--- server.js
+++ server.js
@@ -3,35 +3,52 @@ const Datastore = require('nedb');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
+// init express app
 const app = express();
-const port = 5000;
-
-app.listen(port, () => {
-  console.log('Listening on Port 5000');
+const PORT = process.env.PORT || 3000;
+// Designate port
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
 });
-
+// setting static folder
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+// create database and load
 const database = new Datastore('database.db');
 database.loadDatabase();
-
+// get req for items currently in database
 app.get('/api', (request, response) => {
   database.find({}, (err, data) => {
     response.json();
   });
 });
 
-app.post('/api', (request, response) => {
-  console.log('I got a request');
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
+// get req for weather from darkSky
+app.get('/weather/:latlon', async (request, response) => {
+  const latlon = request.params.latlon.split(',');
+  const lat = latlon[0];
+  const lon = latlon[1];
 
+  sApiKey = process.env.SkyAPIKEY;
+  const api_url = `https://api.darksky.net/forecast/${sApiKey}/${lat},${lon}`;
+  const fetchResponse = await fetch(api_url);
+  const json = await fetchResponse.json();
+  response.json(json);
+});
+
+// posting new data to db
+app.post('/api', async (request, response) => {
+  const data = request.body;
+  const timestamp = new Date();
+  data.timestamp = timestamp.toString();
+
+  const fTime = timestamp.toLocaleString;
+  database.insert(data);
+  // parameters to be sent to db
   response.json({
     status: 'success',
-    time: timestamp,
+    time: fTime,
     lat: data.lat,
     lon: data.lon,
     temp: data.temperature,
@@ -39,16 +56,15 @@ app.post('/api', (request, response) => {
   });
 });
 
-app.get('/weather/:latlon', async (request, response) => {
-  console.log(request.params);
-  const latlon = request.params.latlon.split(',');
-  console.log(latlon);
-  const lat = latlon[0];
-  const lon = latlon[1];
-  console.log(lat, lon);
-  sApiKey = process.env.SkyAPIKEY;
-  const api_url = `https://api.darksky.net/forecast/${sApiKey}/${lat},${lon}`;
-  const fetch_response = await fetch(api_url);
-  const json = await fetch_response.json();
+app.get('/coordinates/:citystate', async (request, response) => {
+  console.log('getting coordinates...');
+  const citystate = request.params.citystate.split(',');
+  const city = citystate[0];
+  const state = citystate[1];
+
+  const googleApiKey = process.env.GMapsAPIKEY;
+  const coords_url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=${googleApiKey}`;
+  const respo = await fetch(coords_url);
+  const json = await respo.json();
   response.json(json);
 });
